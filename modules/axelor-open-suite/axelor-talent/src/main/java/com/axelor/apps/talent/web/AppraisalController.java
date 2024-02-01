446cbf1b04151617cec4fbbcb6207342fd2a7386
Victoria <ricardoperezayon9@gmail.com>
2024-02-01 10:35:06 -0600

    Added appraisal customization, changed logo

--- modules/axelor-open-suite/axelor-talent/src/main/java/com/axelor/apps/talent/web/AppraisalController.java
+++ modules/axelor-open-suite/axelor-talent/src/main/java/com/axelor/apps/talent/web/AppraisalController.java
@@ -31,13 +31,283 @@ import com.axelor.rpc.ActionRequest;
 import com.axelor.rpc.ActionResponse;
 import com.axelor.rpc.Context;
 import com.google.inject.Singleton;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 @Singleton
 public class AppraisalController {
+
+  public static final int defaultFunctional = 70;
+  public static final int defaultProject = 30;
+
+  public void onNew(ActionRequest request, ActionResponse response) {
+    Appraisal appraisal = request.getContext().asType(Appraisal.class);
+
+    response.setValue("functionalPercentage", defaultFunctional);
+    response.setValue("projectPercentage", defaultProject);
+    response.setValue("functionalDivisionsAccum", 100);
+
+    return;
+  }
+
+  public String validateCumulativePercentage(int cumulativePercentage) {
+    String errorMessage = null;
+    if (cumulativePercentage != 100) {
+      errorMessage = "Functional subdivision's percentages do not add to 100%";
+    }
+    return errorMessage;
+  }
+
+  public void onSave(ActionRequest request, ActionResponse response) {
+    Appraisal appraisal = request.getContext().asType(Appraisal.class);
+    String messageString = validateCumulativePercentage(appraisal.getFunctionalDivisionsAccum());
+    if (messageString != null) {
+      response.setError(messageString, "Non-valid value");
+    }
+
+    return;
+  }
+
+  /*
+  public void calculateFunctionalPercentage(ActionRequest request, ActionResponse response) {
+    Appraisal appraisal = request.getContext().asType(Appraisal.class);
+    int projectPercentage = appraisal.getProjectPercentage();
+    if (projectPercentage <= 0 || projectPercentage >= 100) {
+      response.setAlert(
+          "Project and functional percentages should be greater than 0 and less than 100",
+          "Invalid value");
+      return;
+    }
+    int calculatedFunctionalValue = 100 - projectPercentage;
+    response.setValue("functionalPercentage", (Integer) calculatedFunctionalValue);
+    return;
+  }
+
+  public void calculateProjectPercentage(ActionRequest request, ActionResponse response) {
+    Appraisal appraisal = request.getContext().asType(Appraisal.class);
+    int functionalPercentage = appraisal.getFunctionalPercentage();
+    if (functionalPercentage <= 0 || functionalPercentage >= 100) {
+      response.setAlert(
+          "Project and functional percentages should be greater than 0 and less than 100",
+          "Invalid value");
+      return;
+    }
+    int calculatedProjectValue = 100 - functionalPercentage;
+    response.setValue("projectPercentage", (Integer) calculatedProjectValue);
+    return;
+  }
+  */
+
+  public void lockUnlockDivision0(ActionRequest request, ActionResponse response) {
+    Appraisal appraisal = request.getContext().asType(Appraisal.class);
+
+    boolean divisionState = appraisal.getFunctionalDivisionCheck0();
+    if (divisionState) {
+      response.setAttr("functionalDivisionName0", "readonly", false);
+      response.setAttr("functionalDivisionValue0", "readonly", false);
+    } else {
+      response.setAttr("functionalDivisionName0", "readonly", true);
+      response.setValue("functionalDivisionName0", "");
+      response.setAttr("functionalDivisionValue0", "readonly", true);
+      response.setValue("functionalDivisionValue0", 0);
+    }
+    return;
+  }
+
+  public void lockUnlockDivision1(ActionRequest request, ActionResponse response) {
+    Appraisal appraisal = request.getContext().asType(Appraisal.class);
+
+    boolean divisionState = appraisal.getFunctionalDivisionCheck1();
+    if (divisionState) {
+      response.setValue("functionalDivisionName1", "");
+      response.setAttr("functionalDivisionName1", "readonly", false);
+      response.setValue("functionalDivisionValue1", 0);
+      response.setAttr("functionalDivisionValue1", "readonly", false);
+      return;
+    } else {
+      response.setAttr("functionalDivisionName1", "readonly", true);
+      response.setAttr("functionalDivisionValue1", "readonly", true);
+    }
+    return;
+  }
+
+  public void lockUnlockDivision2(ActionRequest request, ActionResponse response) {
+    Appraisal appraisal = request.getContext().asType(Appraisal.class);
+
+    boolean divisionState = appraisal.getFunctionalDivisionCheck2();
+    if (divisionState) {
+      response.setValue("functionalDivisionName2", "");
+      response.setAttr("functionalDivisionName2", "readonly", false);
+      response.setValue("functionalDivisionValue2", 0);
+      response.setAttr("functionalDivisionValue2", "readonly", false);
+      return;
+    } else {
+      response.setAttr("functionalDivisionName2", "readonly", true);
+      response.setAttr("functionalDivisionValue2", "readonly", true);
+    }
+    return;
+  }
+
+  public void lockUnlockDivision3(ActionRequest request, ActionResponse response) {
+    Appraisal appraisal = request.getContext().asType(Appraisal.class);
+
+    boolean divisionState = appraisal.getFunctionalDivisionCheck3();
+    if (divisionState) {
+      response.setValue("functionalDivisionName3", "");
+      response.setAttr("functionalDivisionName3", "readonly", false);
+      response.setValue("functionalDivisionValue3", 0);
+      response.setAttr("functionalDivisionValue3", "readonly", false);
+      return;
+    } else {
+      response.setAttr("functionalDivisionName3", "readonly", true);
+      response.setAttr("functionalDivisionValue3", "readonly", true);
+    }
+    return;
+  }
+
+  public void lockUnlockDivision4(ActionRequest request, ActionResponse response) {
+    Appraisal appraisal = request.getContext().asType(Appraisal.class);
+
+    boolean divisionState = appraisal.getFunctionalDivisionCheck4();
+    if (divisionState) {
+      response.setValue("functionalDivisionName4", "");
+      response.setAttr("functionalDivisionName4", "readonly", false);
+      response.setValue("functionalDivisionValue4", 0);
+      response.setAttr("functionalDivisionValue4", "readonly", false);
+      return;
+    } else {
+      response.setAttr("functionalDivisionName4", "readonly", true);
+      response.setAttr("functionalDivisionValue4", "readonly", true);
+    }
+    return;
+  }
+
+  public void lockUnlockDivision5(ActionRequest request, ActionResponse response) {
+    Appraisal appraisal = request.getContext().asType(Appraisal.class);
+
+    boolean divisionState = appraisal.getFunctionalDivisionCheck5();
+    if (divisionState) {
+      response.setValue("functionalDivisionName5", "");
+      response.setAttr("functionalDivisionName5", "readonly", false);
+      response.setValue("functionalDivisionValue5", 0);
+      response.setAttr("functionalDivisionValue5", "readonly", false);
+      return;
+    } else {
+      response.setAttr("functionalDivisionName5", "readonly", true);
+      response.setAttr("functionalDivisionValue5", "readonly", true);
+    }
+    return;
+  }
+
+  public void lockUnlockDivision6(ActionRequest request, ActionResponse response) {
+    Appraisal appraisal = request.getContext().asType(Appraisal.class);
+
+    boolean divisionState = appraisal.getFunctionalDivisionCheck6();
+    if (divisionState) {
+      response.setValue("functionalDivisionName6", "");
+      response.setAttr("functionalDivisionName6", "readonly", false);
+      response.setValue("functionalDivisionValue6", 0);
+      response.setAttr("functionalDivisionValue6", "readonly", false);
+      return;
+    } else {
+      response.setAttr("functionalDivisionName6", "readonly", true);
+      response.setAttr("functionalDivisionValue6", "readonly", true);
+    }
+    return;
+  }
+
+  public void lockUnlockDivision7(ActionRequest request, ActionResponse response) {
+    Appraisal appraisal = request.getContext().asType(Appraisal.class);
+
+    boolean divisionState = appraisal.getFunctionalDivisionCheck7();
+    if (divisionState) {
+      response.setValue("functionalDivisionName7", "");
+      response.setAttr("functionalDivisionName7", "readonly", false);
+      response.setValue("functionalDivisionValue7", 0);
+      response.setAttr("functionalDivisionValue7", "readonly", false);
+      return;
+    } else {
+      response.setAttr("functionalDivisionName7", "readonly", true);
+      response.setAttr("functionalDivisionValue7", "readonly", true);
+    }
+    return;
+  }
+
+  public void lockUnlockDivision8(ActionRequest request, ActionResponse response) {
+    Appraisal appraisal = request.getContext().asType(Appraisal.class);
+
+    boolean divisionState = appraisal.getFunctionalDivisionCheck8();
+    if (divisionState) {
+      response.setValue("functionalDivisionName8", "");
+      response.setAttr("functionalDivisionName8", "readonly", false);
+      response.setValue("functionalDivisionValue8", 0);
+      response.setAttr("functionalDivisionValue8", "readonly", false);
+      return;
+    } else {
+      response.setAttr("functionalDivisionName8", "readonly", true);
+      response.setAttr("functionalDivisionValue8", "readonly", true);
+    }
+    return;
+  }
+
+  public void lockUnlockDivision9(ActionRequest request, ActionResponse response) {
+    Appraisal appraisal = request.getContext().asType(Appraisal.class);
+
+    boolean divisionState = appraisal.getFunctionalDivisionCheck9();
+    if (divisionState) {
+      response.setValue("functionalDivisionName9", "");
+      response.setAttr("functionalDivisionName9", "readonly", false);
+      response.setValue("functionalDivisionValue9", 0);
+      response.setAttr("functionalDivisionValue9", "readonly", false);
+      return;
+    } else {
+      response.setAttr("functionalDivisionName9", "readonly", true);
+      response.setAttr("functionalDivisionValue9", "readonly", true);
+    }
+    return;
+  }
+
+  public void calculateCumulativeSubpercentage(ActionRequest request, ActionResponse response) {
+    Appraisal appraisal = request.getContext().asType(Appraisal.class);
+
+    int cumulativeSubpercentage = 0;
+    List<Boolean> checkedDivisionsList = new ArrayList<>();
+    checkedDivisionsList.add(appraisal.getFunctionalDivisionCheck0());
+    checkedDivisionsList.add(appraisal.getFunctionalDivisionCheck1());
+    checkedDivisionsList.add(appraisal.getFunctionalDivisionCheck2());
+    checkedDivisionsList.add(appraisal.getFunctionalDivisionCheck3());
+    checkedDivisionsList.add(appraisal.getFunctionalDivisionCheck4());
+    checkedDivisionsList.add(appraisal.getFunctionalDivisionCheck5());
+    checkedDivisionsList.add(appraisal.getFunctionalDivisionCheck6());
+    checkedDivisionsList.add(appraisal.getFunctionalDivisionCheck7());
+    checkedDivisionsList.add(appraisal.getFunctionalDivisionCheck8());
+    checkedDivisionsList.add(appraisal.getFunctionalDivisionCheck9());
+
+    List<Integer> valuesList = new ArrayList<>();
+    valuesList.add(appraisal.getFunctionalDivisionValue0());
+    valuesList.add(appraisal.getFunctionalDivisionValue1());
+    valuesList.add(appraisal.getFunctionalDivisionValue2());
+    valuesList.add(appraisal.getFunctionalDivisionValue3());
+    valuesList.add(appraisal.getFunctionalDivisionValue4());
+    valuesList.add(appraisal.getFunctionalDivisionValue5());
+    valuesList.add(appraisal.getFunctionalDivisionValue6());
+    valuesList.add(appraisal.getFunctionalDivisionValue7());
+    valuesList.add(appraisal.getFunctionalDivisionValue8());
+    valuesList.add(appraisal.getFunctionalDivisionValue9());
+
+    for (int i = 0; i < 10; i++) {
+      if (checkedDivisionsList.get(i)) {
+        cumulativeSubpercentage += valuesList.get(i);
+      }
+    }
+
+    response.setValue("functionalDivisionsAccum", cumulativeSubpercentage);
+    return;
+  }
 
   public void send(ActionRequest request, ActionResponse response) {
 
@@ -45,6 +315,12 @@ public class AppraisalController {
 
     try {
       appraisal = Beans.get(AppraisalRepository.class).find(appraisal.getId());
+
+      String messageString = validateCumulativePercentage(appraisal.getFunctionalDivisionsAccum());
+      if (messageString != null) {
+        response.setError(messageString, "Non-valid value");
+        return;
+      }
 
       Beans.get(AppraisalService.class).send(appraisal);
 
